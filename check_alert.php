7256e61ba8bba0067363d490d43c2f496c6a5e93
Tristanbonifacio <Tristanbonifacio5@gmail.com>
2026-03-18 23:57:50 +0800

    add map feature to website

--- check_alert.php
+++ check_alert.php
@@ -1,5 +1,12 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "working_project_schema");
-$res = $conn->query("SELECT * FROM system_status WHERE id = 1");
+
+// Gidugangan og JOIN para makuha ang coordinates sa staff gikan sa users table
+$query = "SELECT s.*, u.lat, u.lng 
+          FROM system_status s 
+          LEFT JOIN users u ON s.triggered_by = u.full_name 
+          WHERE s.id = 1";
+
+$res = $conn->query($query);
 echo json_encode($res->fetch_assoc());
 ?>
